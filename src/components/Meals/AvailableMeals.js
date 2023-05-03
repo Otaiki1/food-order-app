fb81c918d992704d994128a478a705365c5bac86
Otaiki1 <otaikisadiq2804@gmail.com>
2023-05-03 16:39:53 +0100

    feat: display individual meal components

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -27,11 +29,20 @@ const DUMMY_MEALS = [
 ];
 export default function AvailableMeals() {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+        key={meal.id}
+      />
+    );
   });
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 }
