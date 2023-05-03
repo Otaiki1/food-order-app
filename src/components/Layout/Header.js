e3fa27d67b72e80d316740373b9062a9354eee00
Otaiki1 <otaikisadiq2804@gmail.com>
2023-05-03 15:08:11 +0100

    feat: add Header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+export default function Header(props) {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>OT Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Table of food" />
+      </div>
+    </>
+  );
+}
