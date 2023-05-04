0a055e2308753bc9579f6dd71526b7c8e0e56542
Otaiki1 <otaikisadiq2804@gmail.com>
2023-05-04 12:17:41 +0100

    refac: wireup useeffevt for animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 export default function HeaderCartButton(props) {
+  const [btnLighted, setBtnLighted] = useState(false);
   const cartCtx = useContext(CartContext);
-
-  const numberOfCartItems = cartCtx.items.reduce(
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce(
     (currNumber, item) => currNumber + item.amount,
     0
   );
 
+  const buttonClasses = `${classes.button} ${btnLighted ? classes.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnLighted(true);
+    const timer = setTimeout(() => {
+      setBtnLighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
